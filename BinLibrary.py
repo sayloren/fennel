maybe have the correct calculation fon for probability...check with Ruth. now also graph the element and random range

BinLibrary.py
@@ -19,9 +19,8 @@ def runProbability(yrange,prange):
 	equal = []
 	for y,p in zip(yrange,prange):
 		if y >= 0:
-			t = y * p #+1.0 # include 0% don't think this is needed
+			t = y + 1.0 # include 0% don't think this is needed
 			k = t**2.0 # probability space
-			print t,k,t/k
 			equal.append(t/k)
 		else:
 			equal.append(0)
@@ -29,37 +28,31 @@ def runProbability(yrange,prange):
 
 def graphComb(n,emp,ranemp,paramlabels):
 # https://betterexplained.com/articles/understanding-the-birthday-paradox/
-	yrange = np.arange(0,n*2)
+	yrange = np.arange(1,n*2)
 	prange = np.empty(n*2)
 	prange.fill(0.5)
 	equal = runProbability(yrange,prange)
-	
-	# One element short
-# 	empequalmin = runProbability(yrange,emp['Min'])
-# 	empequalmax = runProbability(yrange,emp['Max'])
-# 	
-# 	ranempequalmin = runProbability(yrange,ranemp['Min'])
-# 	ranempequalmax = runProbability(yrange,ranemp['Max'])
 	
 	sns.set_style('ticks')
 	sns.set_palette("husl",n_colors=8)#(len(nucLine)*2)
 
 	plt.figure(figsize=(3.5,3.5))
-	plt.plot(yrange,equal,linewidth=2,alpha=0.9)#,color='#3e1638'
-# 	plt.fill_between(yrange,empequalmin,empequalmax,alpha=0.2)
-# 	plt.fill_between(yrange,ranempequalmin,ranempequalmax,alpha=0.2)
+	plt.plot(yrange,equal,linewidth=2,alpha=0.9,label='Theoretical')
+	plt.fill_between(yrange,emp['Min'],emp['Max'],alpha=0.2,label='Observed Range')
+	plt.fill_between(yrange,ranemp['Min'],ranemp['Max'],alpha=0.2,label='Random Range')
 	plt.axvline(x=n,linewidth=.05,linestyle=':')#label='{:0.1e}'.format(equal[n])
 	plt.axvspan(n-1,n+1,alpha=0.1)#,facecolor='#e7298a'
 	plt.xlabel('Bin Size',size=12)
 	plt.ylabel('Probability',size=12)
 	plt.title('Equal Boundary for {0} Bins'.format(n),size=16)
-# 	plt.legend(loc=2,fontsize=12,labelspacing=0.05)
-	plt.text(n-9,.9,'{:0.1e}'.format(equal[n]),size=12,clip_on=False)
+	plt.legend(loc=0,fontsize=6,labelspacing=0.05)
+	plt.text(n-9,.5,'{:0.1e}'.format(equal[n]),size=12,clip_on=False)
 	plt.tight_layout()
 	sns.despine()
 	plt.savefig('Probability_{0}.pdf'.format(paramlabels))
 
 def main(n,emp,ranemp,paramlabels):
+	print 'Running BinLibrary'
 	graphComb(n,emp,ranemp,paramlabels)
 
 if __name__ == "__main__":