4be8390a3eea3e8b6644ef7695cc5130f3d6555e
Wren <sayloren@gmail.com>
2017-10-02 18:36:13 -0400

    removed some extra processing from the compressed methylation pipeline

--- boundarymethylation.py
+++ boundarymethylation.py
@@ -6,6 +6,10 @@ September 19 2017
 
 To Do:
 unittest
+check frequency calculation
+change directionality arg column
+streamline and remove excess columns
+.index len to break up large datasets
 
 May be graphing incorrect value; add frequency and add cpg count before dividing
 
@@ -229,71 +233,9 @@ def compare_boundaries_size_n(element,size):
 # With the results from compare_boundaries_size_n per each element, evaluate directionality into new column
 def evaluate_boundaries_size_n(rangeFeatures,fileName):
 	rangeFeatures['compareBoundaries'] = rangeFeatures.apply(lambda row: (compare_boundaries_size_n(row['feature'],binDir)),axis=1)
-	rangeFeatures['compareBoundariesRange'] = rangeFeatures.apply(lambda row: (emperical_boundaries_for_increasing_bins_symbol(row['feature'])),axis=1)
-	rangeFeatures['equalBoundariesCount'] = rangeFeatures.apply(lambda row: row['compareBoundariesRange'].count('='),axis=1)
-	rangeFeatures['plusBoundariesCount'] = rangeFeatures.apply(lambda row: row['compareBoundariesRange'].count('+'),axis=1)
-	rangeFeatures['minusBoundariesCount'] = rangeFeatures.apply(lambda row: row['compareBoundariesRange'].count('-'),axis=1)
-# 	print rangeFeatures[['plusBoundariesCount','minusBoundariesCount','equalBoundariesCount']]
-	
 	compareEnds = pd.DataFrame(rangeFeatures[['chr','start','end','compareBoundaries']])
 	print 'Sorting the element boundaries by bin size {0}'.format(binDir)
-	rangeBins = collect_emperical_boundary_comparisons(rangeFeatures)
-	return rangeFeatures,rangeBins
-
-# Get the actual spread of = in the data over increasing bin size
-def collect_emperical_boundary_comparisons(rangeFeatures):
-	# Perform min,max collection
-	rangeAT = rangeFeatures.apply(lambda row: (emperical_boundaries_for_increasing_bins_percentage(row['feature'])),axis=1)
-	pdrangeAT = pd.DataFrame(rangeAT.values.tolist())
-	
-	# Get direction collection
-	equalAT = rangeFeatures.apply(lambda row: (emperical_boundaries_for_increasing_bins_symbol(row['feature'])),axis=1)
-	pdequalAT = pd.DataFrame(equalAT.values.tolist())
-	countAT = pdequalAT.apply(pd.value_counts)
-	equalCounts = countAT.loc['=']/len(pdequalAT.index)
-	
-	# Separate Start from End for min and max
-	splitAT = pd.concat(dict([(row[0],row[1].apply(lambda y: pd.Series(y))) for row in pdrangeAT.iterrows()]),axis=1)
-	
-	outcollect = []
-	for column in splitAT:
-		outcollect.append(splitAT[column[0]])
-	outcat = pd.concat(outcollect,axis=1)
-	outcat /= 100 # convert to decimal
-
-	# Separate data frames by Start v End and Min v Max
-	pdATCollectStartMin = outcat[[outcat.columns[0]]].min(axis=1)
-	pdATCollectEndMin = outcat[[outcat.columns[1]]].min(axis=1)
-	
-	pdATCollectStartMax = outcat[[outcat.columns[0]]].max(axis=1)
-	pdATCollectEndMax = outcat[[outcat.columns[1]]].max(axis=1)
-	
-	Min = pdATCollectStartMin * pdATCollectEndMin
-	Max = pdATCollectStartMax * pdATCollectEndMax
-
-	pdBins = pd.concat([Min,Max,equalCounts],axis=1)
-	pdBins.columns=['Min','Max','Equal']
-	return pdBins
-
-# Run calculate_nucleotides_at for increasing bin sizes
-def emperical_boundaries_for_increasing_bins_percentage(element):
-	totalSteps=[]
-	for i in np.arange(1,(uce/2)):
-		pairStep = calculate_nucleotides_at(element,i)
-		totalSteps.append(pairStep)
-	return totalSteps
-
-# Run compare_boundaries_size_n over increasing bin sizes
-def emperical_boundaries_for_increasing_bins_symbol(element):
-	totalSteps=[]
-	for i in np.arange(1,(uce/2)):
-		perSize = calculate_nucleotides_at(element,i)
-		# give + - = depending on which side has larger AT content
-		if perSize[0] > perSize[1]: outList = '+'
-		if perSize[1] > perSize[0]: outList = '-'
-		if perSize[1] == perSize[0]: outList = '='
-		totalSteps.append(outList)
-	return totalSteps
+	return rangeFeatures
 
 # Threshold methylation data by coverage and percentage
 def threshold_methylation_data(methFeatures):
@@ -505,7 +447,7 @@ def main():
 	
 	# Get coords and strings for elements
 	rangeFeatures = collect_element_coordinates(eFiles)
-	directionFeatures,directionBins = evaluate_boundaries_size_n(rangeFeatures,eFiles)
+	directionFeatures = evaluate_boundaries_size_n(rangeFeatures,eFiles)
 	
 	# Get the probability for each directional assignment, and use to randomly assign the correct number of random directions
 	dirOptions = ['-','+','=']
@@ -539,7 +481,7 @@ def main():
 		if rFiles:
 			for randomFile in rFiles:
 				randomFeatures = collect_element_coordinates(randomFile)
-				randirFeatures,randirBins = evaluate_boundaries_size_n(randomFeatures,randomFile)
+				randirFeatures= evaluate_boundaries_size_n(randomFeatures,randomFile)
 				allrandommethylationupstream,allrandommethylationdownstream = collect_methylation_data_by_element(randirFeatures)
 				allrandommethylationupstream['group'] = 'random'
 				allrandommethylationdownstream['group'] = 'random'
@@ -598,7 +540,7 @@ def main():
 			if rFiles:
 				for randomFile in rFiles:
 					randomFeatures = collect_element_coordinates(randomFile)
-					randirFeatures,randirBins = evaluate_boundaries_size_n(randomFeatures,randomFile)
+					randirFeatures= evaluate_boundaries_size_n(randomFeatures,randomFile)
 					rantypeBool,randomtypeupstreammethylation,randomtypedownstreammethylation = separate_dataframe_by_group(type,randirFeatures,'type',randomFile)
 					randomtypeupstreammethylation['group'] = 'random'
 					randomtypedownstreammethylation['group'] = 'random'
